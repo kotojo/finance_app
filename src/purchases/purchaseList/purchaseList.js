826c779949c55114e135ab1762d6901d888b77a1
Bruce Kellerman <brucek17@hotmail.com>
2017-02-03 18:20:24 +0000

    fixed issue where I forgot to refactor a few functions i removed bind events from, and bug where editing date didn't save

--- src/purchases/purchaseList/purchaseList.js
+++ src/purchases/purchaseList/purchaseList.js
@@ -1,34 +1,70 @@
 import React, { Component } from 'react'
+import './purchaseList.css'
 
 class PurchaseList extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { activePurchase: null, cost: null, type: null }
-    this.setActive = this.setActive.bind(this)
-    this.handleCostChange = this.handleCostChange.bind(this)
-    this.handleSelectChange = this.handleSelectChange.bind(this)
-    this.handlePurchaseUpdate = this.handlePurchaseUpdate.bind(this)
+  state = {
+    activePurchase: null,
+    cost: null,
+    type: null,
+    date: '',
+    hasError: {
+      cost: false,
+      date: false
+    }
   }
 
-  handleCostChange (event) {
+  handleCostChange = (event) => {
     let regex = /^\d+(\.|,)\d{2}$/
     this.setState({ cost: event.target.value,
-      hasError: !regex.test(event.target.value) })
+      hasError: {
+        cost: !regex.test(event.target.value),
+        date: this.state.hasError.date
+      }
+    })
   }
 
-  handleSelectChange (event) {
+  handleDateChange = (event) => {
+    const validDate = this.isValidDate(event.target.value)
+    this.setState({ date: event.target.value,
+      hasError: { date: !validDate,
+        cost: this.state.hasError.cost }
+    })
+  }
+
+  isValidDate (dateStr) {
+    if (dateStr == null) return false
+    const date = new Date(dateStr)
+    return date.getTime() > (new Date('1900-01-01')).getTime()
+  }
+
+  handleSelectChange = (event) => {
     this.setState({type: event.target.value})
   }
 
-  handlePurchaseUpdate () {
-    this.props.updatePurchase(this.state.cost, this.state.type, this.state.activePurchase)
+  handlePurchaseUpdate = () => {
+    const { cost } = this.state.hasError
+    const validDate = this.isValidDate(this.state.date)
+    if (!validDate || cost) {
+      this.setState({
+        hasError: {
+          date: !validDate,
+          cost
+        }
+      })
+      return
+    }
+    this.props.updatePurchase(this.state.cost, this.state.type, this.state.date, this.state.activePurchase)
     this.setState({activePurchase: null})
   }
 
-  setActive (key) {
+  setActive = (key) => {
     if (key === undefined) this.setState({ activePurchase: null, cost: null, type: null })
     else {
-      this.setState({activePurchase: key, cost: this.props.purchases[key].cost, type: this.props.purchases[key].type})
+      this.setState({activePurchase: key,
+        cost: this.props.purchases[key].cost,
+        type: this.props.purchases[key].type,
+        date: this.props.purchases[key].date
+      })
     }
   }
 
@@ -37,9 +73,9 @@ class PurchaseList extends Component {
       if (key !== this.state.activePurchase) {
         return (
           <li key={key}>
-            <span>{this.props.purchases[key].type}: {this.props.purchases[key].cost}</span>
-            <button onClick={() => this.setActive(key)}>Edit</button>
-            <button onClick={() => this.props.removePurchase(key)}>Delete</button>
+            <span>{this.props.purchases[key].type}: {this.props.purchases[key].cost} - {this.props.purchases[key].date}</span>
+            <button className='listItemBtn' onClick={() => this.setActive(key)}>Edit</button>
+            <button className='listItemBtn' onClick={() => this.props.removePurchase(key)}>Delete</button>
           </li>
         )
       } else {
@@ -53,9 +89,11 @@ class PurchaseList extends Component {
               <option value='entertainment'>Entertainment</option>
             </select>$
             <input onChange={this.handleCostChange} value={this.state.cost}
-              type='text' name='purchaseCost' />
-            <button onClick={this.handlePurchaseUpdate}>Save</button>
-            <button onClick={() => this.setActive()}>Cancel</button>
+              type='text' name='purchaseCost' className={this.state.hasError.cost ? 'error' : ''} />
+            <input onChange={this.handleDateChange} value={this.state.date}
+              type='date' name='purchaseDate' className={this.state.hasError.date ? 'error' : ''} />
+            <button className='listItemBtn' onClick={this.handlePurchaseUpdate}>Save</button>
+            <button className='listItemBtn' onClick={() => this.setActive()}>Cancel</button>
           </li>
         )
       }
--- src/purchases/purchases.js
+++ src/purchases/purchases.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import PurchaseForm from './purchaseForm/purchaseForm'
 import PurchaseList from './purchaseList/purchaseList'
+import PurchaseChart from './purchaseChart/purchaseChart'
+import './purchases.css'
 const firebase = window.firebase
 
 class Purchases extends Component {
+  state = { purchases: {} }
+
   constructor (props) {
     super(props)
-    this.state = { purchases: {} }
-    this.addPurchase = this.addPurchase.bind(this)
-    this.removePurchase = this.removePurchase.bind(this)
-    this.updatePurchase = this.updatePurchase.bind(this)
     this.purchaseSubscription = firebase.database().ref(`${this.props.userId}/purchases/`).on('value', snapshot => {
-      let purchases = snapshot.val() || {}
+      const purchases = snapshot.val() || {}
       this.setState({purchases})
     })
   }
@@ -20,13 +20,13 @@ class Purchases extends Component {
     firebase.database().ref(`${this.props.userId}/purchases/`).off('value', this.purchaseSubscription)
   }
 
-  addPurchase (cost, type) {
-    let purchaseRef = firebase.database().ref(`${this.props.userId}/purchases/`)
-    let newPurchase = purchaseRef.push()
-    newPurchase.set({cost, type})
+  addPurchase = (cost, type, date) => {
+    const purchaseRef = firebase.database().ref(`${this.props.userId}/purchases/`)
+    const newPurchase = purchaseRef.push()
+    newPurchase.set({cost, type, date})
   }
 
-  removePurchase (key) {
+  removePurchase = (key) => {
     const purchase = firebase.database().ref(`${this.props.userId}/purchases/${key}`)
     purchase.remove()
       .then(() => {
@@ -36,10 +36,10 @@ class Purchases extends Component {
       })
   }
 
-  updatePurchase (cost, type, id) {
-    let regex = /^\d+(\.|,)\d{2}$/
+  updatePurchase = (cost, type, date, id) => {
+    const regex = /^\d+(\.|,)\d{2}$/
     if (!regex.test(cost)) return
-    let purchase = {cost, type}
+    const purchase = {cost, type, date}
     firebase.database().ref(`${this.props.userId}/purchases/${id}`).update(purchase)
   }
 
@@ -47,8 +47,11 @@ class Purchases extends Component {
     return (
       <div>
         <PurchaseForm addPurchase={this.addPurchase} />
-        <PurchaseList purchases={this.state.purchases} removePurchase={this.removePurchase}
-          updatePurchase={this.updatePurchase} />
+        <div className='flex-container'>
+          <PurchaseList purchases={this.state.purchases}
+            removePurchase={this.removePurchase} updatePurchase={this.updatePurchase} />
+          <PurchaseChart purchases={this.state.purchases} />
+        </div>
       </div>
     )
   }
